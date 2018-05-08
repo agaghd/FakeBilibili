ac26b1b02f5a20b72b5dc592d1859a4529ef9bb3
agaghd <15150829710@163.com>
2018-05-08 22:52:29 +0800

    添加API解析工具和单元测试方法。

---
+++ basemodel/src/main/java/io/github/agaghd/basemodel/utils/APIAnalyzeUtil.java
@@ -0,0 +1,24 @@
+package io.github.agaghd.basemodel.utils;
+
+/**
+ * author : wjy
+ * time   : 2018/05/08
+ * desc   : API 分析工具
+ */
+
+public class APIAnalyzeUtil {
+
+    public static String splitAPI(String sourceUrl){
+        StringBuilder sb = new StringBuilder();
+        if (sourceUrl!=null){
+            String[] sourceArray = sourceUrl.split("&");
+            if (sourceArray.length>0){
+                for (String s:sourceArray){
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
