2ddecb980b92ed49f154745dce331eaefc38a7d9
agaghd <15150829710@163.com>
2018-05-22 15:11:04 +0800

    首页引入TabLayout。

--- app/src/main/java/io/github/agaghd/fakebilibili/mainpage/MainFragment.java
+++ app/src/main/java/io/github/agaghd/fakebilibili/mainpage/MainFragment.java
@@ -2,11 +2,16 @@ package io.github.agaghd.fakebilibili.mainpage;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.github.agaghd.fakebilibili.R;
 
 /**
@@ -17,10 +22,43 @@ import io.github.agaghd.fakebilibili.R;
 
 public class MainFragment extends Fragment {
 
+    @Bind(R.id.main_tab)
+    TabLayout mainTab;
+    @Bind(R.id.main_view_pager)
+    ViewPager mainViewPager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, root);
+        setUpListener();
         return root;
+    }
+
+    private void setUpListener() {
+        mainTab.setupWithViewPager(mainViewPager);
+        mainTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(getActivity(), tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
