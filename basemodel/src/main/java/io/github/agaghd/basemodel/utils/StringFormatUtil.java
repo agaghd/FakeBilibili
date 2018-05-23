f72502e8b600afc8a1d0ca91ba40e61a2e4b7780
agaghd <15150829710@163.com>
2018-05-23 14:39:18 +0800

    字符串工具类添加用于转换时间格式为HH：mm：ss格式的工具方法。

--- basemodel/src/main/java/io/github/agaghd/basemodel/utils/StringFormatUtil.java
+++ basemodel/src/main/java/io/github/agaghd/basemodel/utils/StringFormatUtil.java
@@ -2,6 +2,9 @@ package io.github.agaghd.basemodel.utils;
 
 import android.text.TextUtils;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 /**
  * author : wjy
  * time   : 2018/05/23
@@ -24,6 +27,23 @@ public class StringFormatUtil {
             }
         }
         return result;
+    }
+
+    public static String getHMSTimeString(long time) {
+        NumberFormat numberFormat = new DecimalFormat("00");
+        long s = time % 60;//秒
+        long m = (time % 3600) / 60;//分
+        long h = time / 3600;//时
+        String st = numberFormat.format(s);
+        String mt = numberFormat.format(m);
+        String ht = numberFormat.format(h);
+        String timeString;
+        if (h > 0) {
+            timeString = ht + ":" + mt + ":" + st;
+        } else {
+            timeString = mt + ":" + st;
+        }
+        return timeString;
     }
 
     private StringFormatUtil() {
